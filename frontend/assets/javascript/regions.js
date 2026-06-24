04cc32cc5f4f05c4f08fd4ff29b5981e18510c3b
kovzadzem <Kovzadzeemariam@gmail.com>
2026-06-24 23:26:08 +0400

    add password recovery page layout
    - created forgot password page UI
    - added responsive white container design
    - improved input and button styling for better UX

---
+++ frontend/assets/javascript/regions.js
@@ -0,0 +1,13 @@
+const info = document.getElementById("info");
+
+document.getElementById("kakheti").onclick = () => {
+  info.innerText = "კახეთი - ჩურჩხელა 🍇";
+};
+
+document.getElementById("imereti").onclick = () => {
+  info.innerText = "იმერეთი - ხაჭაპური 🧀";
+};
+
+document.getElementById("adjara").onclick = () => {
+  info.innerText = "აჭარა - აჭარული ხაჭაპური 🍳";
+};
